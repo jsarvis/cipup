d4519a3d0758a54ecb7764d541ef0a1a970daedb
Jacob Sarvis <jacob.sarvis@gmail.com>
2009-06-12 01:24:13 +0100

    Initial directory structure

---
+++ cipup/src/cipup.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : cipup.cpp
+// Author      : Jacob Sarvis
+// Version     :
+// Copyright   : Copyright 2009 Jacob Sarvis
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	puts("Welcome to CIPUP");
+	cout << "Cipup Is Privacy for the Ultra Paranoid" << endl;
+
+	cout << "Press Enter to continue . . .\n";
+	getchar();
+
+	return EXIT_SUCCESS;
+}
